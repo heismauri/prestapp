6a8b5d8f6351320ae7d72c158d462205949bb182
Mauricio Villa <vbamauricio@gmail.com>
2021-09-03 18:05:01 -0400

    Cambios en el navbar, permisos usuarios e indentacion

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @order = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :articles
   resources :categories, only: %i[index show]
+  resources :orders, only: %i[show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
