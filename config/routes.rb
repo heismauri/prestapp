7a3929f5a3bb227b97671f20ba6ff076ddea4768
Orlando Altamiranda <orlandoaltamiranda@hotmail.com>
2021-09-21 16:41:20 -0300

    Cambios front en formularios, botones y correciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path: ''
   root to: 'pages#home'
-  resources :pages, only: %i[show], path: '', param: :page
   resources :articles
   get '/search', to: 'articles#search'
   resources :categories, only: %i[index show]
   resources :orders, only: %i[index edit update]
   resources :order_items, only: %i[create destroy]
-  resources :order_details, only: %i[create destroy]
+  resources :order_details, only: %i[create destroy] 
+  resources :pages, only: %i[show], path: '', param: :page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
